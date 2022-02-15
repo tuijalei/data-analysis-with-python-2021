b0d61445d48652291f89f90c335549e4d7078b46
Tuija Leinonen <tuijaleinonen20@gmail.com>
2022-02-15 19:29:08 +0200

    Update part02-e13_diamond.py

--- week 2/part02-e13_diamond.py
+++ week 2/part02-e13_diamond.py
@@ -3,16 +3,10 @@ import numpy as np
 
 def diamond(n):
     a, b = np.eye(n, dtype=int), np.eye(n, dtype=int)[:,::-1]
-    print(a)
-    print()
-    print(b)
-
-    right, left = np.concatenate((a,b)), np.concatenate((b,a))
-
-    diamond = np.hstack((left, right[:, 1:]))
-    c, d = np.hstack((b,a[:,1:])), np.hstack((a,b[:,1:]))
-
-    return np.vstack((c, d[1:,:]))
+    top = np.concatenate((b,a[:, 1:]), axis=1)
+    bottom = np.concatenate((a,b[:, 1:]), axis=1)
+    diamond = np.concatenate((top, bottom[1:]), axis=0)
+    return diamond
     
 def main():
     pass
